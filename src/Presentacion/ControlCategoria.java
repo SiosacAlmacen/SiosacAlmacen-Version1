bf9556fd2be26aa375cb4f9faa0dcb7c9ed682c1
Anderson <andersonsurco5@gmail.com>
2020-12-08 23:56:25 -0500

    Añadiendo vista y control de categoria (Presentacion)

---
+++ src/Presentacion/ControlCategoria.java
@@ -0,0 +1,147 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Presentacion;
+
+import Negocio.Categoria;
+import Servicios.ServicioCategoria;
+import Servicios.ServicioCategoriaImp;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author ander
+ */
+public class ControlCategoria  implements ActionListener{
+    
+    ServicioCategoria SerCat = new ServicioCategoriaImp();
+    Categoria cat = new Categoria();
+    
+    FrmCategoria FrmCat = new FrmCategoria();
+    DefaultTableModel Tmodel = new DefaultTableModel();
+    
+    public ControlCategoria(FrmCategoria FrmCat) {
+        this.FrmCat = FrmCat;
+        this.FrmCat.btnleer.addActionListener(this);
+        this.FrmCat.btnactualizar.addActionListener(this);
+        this.FrmCat.btneditar.addActionListener(this);
+        this.FrmCat.btneliminar.addActionListener(this);
+        this.FrmCat.btnregistrar.addActionListener(this);
+        this.FrmCat.btnlimpiar.addActionListener(this);
+        Read(FrmCat.tabla);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+        if (e.getSource() == FrmCat.btnleer) {
+            CleanTable();
+            Read(FrmCat.tabla);
+        }
+        if (e.getSource() == FrmCat.btnregistrar) {
+            Create();
+        }
+        if (e.getSource() == FrmCat.btneditar) {
+            Edit();
+        }
+        if (e.getSource() == FrmCat.btnactualizar) {
+            Update();
+        }
+        if (e.getSource() == FrmCat.btneliminar) {
+            Delete();
+        }
+        if (e.getSource() == FrmCat.btnlimpiar) {
+            Clean();
+        }
+        
+    }
+    
+    public void Create(){
+        if (!"".equals(FrmCat.txtcodigo.getText()) && !"".equals(FrmCat.txtnombre.getText())) {
+            String codigo = FrmCat.txtcodigo.getText();
+            String nombre = FrmCat.txtnombre.getText();
+            String msg = SerCat.Create(codigo,nombre);
+            JOptionPane.showMessageDialog(null,msg);
+            CleanTable();
+            Read(FrmCat.tabla);
+        }else{
+            JOptionPane.showMessageDialog(null,"Complete todos los campos");
+        }
+    }
+    
+    public void Read(JTable tabla){
+        Tmodel = (DefaultTableModel)tabla.getModel();
+        for (int i = 1; i < SerCat.Read().size(); i++){
+            Object[] fila = (Object[])SerCat.Read().get(i);
+            Tmodel.addRow(fila);
+        }
+        FrmCat.tabla.setModel(Tmodel);
+    }
+    
+    public void Update(){
+        if (!"".equals(FrmCat.txtcodigo.getText()) && !"".equals(FrmCat.txtnombre.getText())) {
+            String codigo = FrmCat.txtcodigo.getText();
+            String nombre = FrmCat.txtnombre.getText();
+            String msg = SerCat.Update(codigo, nombre);
+            JOptionPane.showMessageDialog(null,msg);
+            Clean();
+            CleanTable();
+            Read(FrmCat.tabla);
+        }else{
+            JOptionPane.showMessageDialog(null,"Complete todos los campos");
+        }
+    }
+    
+    public void Delete(){
+        if (!"".equals(FrmCat.txtcodigo.getText()) && !"".equals(FrmCat.txtnombre.getText())) {
+        String codigo = FrmCat.txtcodigo.getText();
+        String msg = SerCat.Delete(codigo);
+        JOptionPane.showMessageDialog(null,msg);
+        CleanTable();
+        Read(FrmCat.tabla);
+        }else{
+            JOptionPane.showMessageDialog(null,"Seleccione un elemento de la lista");
+        }
+    }
+    
+    public void Edit(){
+        int element = FrmCat.tabla.getSelectedRow();
+        if(element == -1){
+            JOptionPane.showMessageDialog(null, "Selecciones un registro a editar");
+        }else{
+        FrmCat.txtcodigo.setText(""+ FrmCat.tabla.getValueAt(element, 0));
+        FrmCat.txtnombre.setText(""+ FrmCat.tabla.getValueAt(element, 1));
+        }
+    }
+   
+    public void CleanTable(){
+        for (int i = 0; i < FrmCat.tabla.getRowCount(); i++) {
+            Tmodel.removeRow(i);
+            i = i-1;
+        }
+    }
+    
+    public void CentrarCeldas(JTable tabla){
+        DefaultTableCellRenderer dtc = new DefaultTableCellRenderer();
+        dtc.setHorizontalAlignment(SwingConstants.CENTER);
+        
+        for (int i = 0; i < tabla.getColumnCount(); i++) {
+            tabla.getColumnModel().getColumn(i).setCellRenderer(dtc);
+        }
+        
+    }
+  
+    public void Clean(){
+        FrmCat.txtcodigo.setText("");
+        FrmCat.txtnombre.setText("");
+        FrmCat.txtcodigo.requestFocus();
+    }
+}
